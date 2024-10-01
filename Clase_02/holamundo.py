32b1caa061a5cd071c280127e239c539ea2493ab
Jorge Martinez <tnlcomputer@gmail.com>
2024-10-01 10:46:07 -0300

    first commit

---
+++ Clase_02/holamundo.py
@@ -0,0 +1,12 @@
+print('Hola mundo!!!')
+
+
+producto1,producto2,producto3 = 'manzanas','peras','limones'
+print('\n',producto1,'\n',producto2,'\n',producto3,'\n')
+
+# Promedio de meses
+enero, febrero, marzo, abril, mayo, junio = 100, 200, 500, 750, 200, 690
+promedio = (enero + febrero + marzo + abril + mayo + junio)/6
+print("El promedio semestral es de: ", promedio)
+
+
